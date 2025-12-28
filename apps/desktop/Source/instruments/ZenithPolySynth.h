d22f4017570b694f8ace2c43a5934ecfd0ccee36
Micah <micah@pop-os.lan>
2025-12-28 12:20:27 -0800

    fix: make modMatrixLock_ mutable for const method access
    Allows locking in const getModulationMatrix() method while maintaining
    const correctness of the interface.

--- apps/desktop/Source/instruments/ZenithPolySynth.h
+++ apps/desktop/Source/instruments/ZenithPolySynth.h
@@ -172,7 +172,7 @@ public:
 
 private:
   juce::SpinLock voiceLock_;
-  juce::SpinLock modMatrixLock_;
+  mutable juce::SpinLock modMatrixLock_;
   juce::MPESynthesiser synthesiser_;
   std::atomic<double> currentBpm_{120.0};
   juce::AudioProcessorValueTreeState parameters_;
